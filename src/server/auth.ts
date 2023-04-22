030959af47c42aada6a9d318eabd927e6778ee2a
jjwebsolutions <123169627+jjwebsolutions@users.noreply.github.com>
2023-04-22 17:40:23 +0200

    v0.0.5

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -4,7 +4,6 @@ import {
   type NextAuthOptions,
   type DefaultSession,
 } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
 import { CredentialsProvider } from "next-auth/providers";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { env } from "~/env.mjs";
