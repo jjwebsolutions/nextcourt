78bb4096e912e33ce7d62ab1041a55eb608937dc
jjwebsolutions <123169627+jjwebsolutions@users.noreply.github.com>
2023-04-25 03:55:12 +0200

    responsive

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import { useSession, signIn, signOut } from "next-auth/react";
-import { Menu } from "@headlessui/react";
+import { Popover, Transition } from "@headlessui/react";
 export default function Header() {
   const { data: session } = useSession();
 
@@ -14,14 +14,14 @@ export default function Header() {
   if (session) {
     return (
       <>
-        <Menu>
+        <Popover className="relative">
           <header>
             <nav className=" mb-5 flex items-center justify-between p-4">
               <Link href="/" className="text-4xl  font-black text-darkest">
                 Tennis Court
               </Link>
 
-              <Menu.Button>
+              <Popover.Button>
                 <button className="text-teal-200 border-teal-400 flex items-center rounded border px-3 py-2 hover:border-white hover:text-white sm:hidden">
                   <svg
                     className="h-3 w-3 fill-current"
@@ -32,11 +32,11 @@ export default function Header() {
                     <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
                   </svg>
                 </button>
-              </Menu.Button>
+              </Popover.Button>
 
               <ul className="hidden sm:flex">
-                <li className="mr-4 pt-2">
-                  <Link href="/reserve" className="btn p-3">
+                <li className="mr-4 pt-2 ">
+                  <Link href="/reserve" className="btn p-3 ">
                     Sessions
                   </Link>
                 </li>
@@ -52,33 +52,44 @@ export default function Header() {
                 </li>
               </ul>
             </nav>
-            <Menu.Items>
-              <ul className="flex flex-col items-center sm:hidden ">
-                <Menu.Item>
+            <Transition
+              enter="transition duration-100 ease-out"
+              enterFrom="transform scale-95 opacity-0"
+              enterTo="transform scale-100 opacity-100"
+              leave="transition duration-75 ease-out"
+              leaveFrom="transform scale-100 opacity-100"
+              leaveTo="transform scale-95 opacity-0"
+            >
+              <Popover.Panel>
+                <ul className="flex flex-col items-center sm:hidden ">
                   <li className="mr-4 p-2">
-                    <Link href="/reserve" className="p-3">
+                    <Link
+                      href="/reserve"
+                      className="p-3 text-xl  font-bold text-darkest"
+                    >
                       Sessions
                     </Link>
                   </li>
-                </Menu.Item>
-                <Menu.Item>
+
                   <li className="mr-4  p-2">
-                    <Link href="/profile" className=" p-3">
+                    <Link
+                      href="/profile"
+                      className="p-3 text-xl font-bold  text-darkest"
+                    >
                       Profile
                     </Link>
                   </li>
-                </Menu.Item>
-                <Menu.Item>
-                  <li className="mr-4 p-2">
+
+                  <li className="mr-4 p-3 text-xl font-bold text-darkest">
                     <button className="" onClick={() => void handleSignOut()}>
                       Sign out
                     </button>
                   </li>
-                </Menu.Item>
-              </ul>
-            </Menu.Items>
+                </ul>
+              </Popover.Panel>
+            </Transition>
           </header>
-        </Menu>
+        </Popover>
       </>
     );
   }
